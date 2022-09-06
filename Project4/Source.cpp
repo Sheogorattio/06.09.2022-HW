4048ec27d993060ba8b894ef4241e939aeb6f967
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2022-09-06 19:50:54 +0300

    +class Point( Set/Get methods)

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -7,7 +7,26 @@ private:
 	int y;
 	int z;
 public:
+	void SetX(int new_x) {
+		x = new_x;
+	}
+	int GetX() {
+		return x;
+	}
 
+	void SetY(int new_y) {
+		y = new_y;
+	}
+	int GetY() {
+		return y;
+	}
+
+	void SetZ(int new_z) {
+		z = new_z;
+	}
+	int GetZ() {
+		return z;
+	}
 };
 
 int main() {
